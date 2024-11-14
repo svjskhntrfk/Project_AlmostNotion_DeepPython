7dedf85c91c706c567b6870e43748ed4ec9eca8b
siehfis <mdfilippovich@edu.hse.ru>
2024-11-14 15:39:43 +0300

    initial commit

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from fastapi import FastAPI
 from db import create_tables, delete_tables
 from router import router as users_router
 
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import HTMLResponse
+from starlette.requests import Request
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await delete_tables()
@@ -15,3 +20,11 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(users_router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("entry.html", {"request": request})
