ceac048eacc900b56530069e14ddba418c6f5ce9
Arina Bazukina <arinabazukina@yandex.ru>
2024-11-14 15:10:00 +0300

    Popytka_db

---
+++ database_1.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+
+
+def init_db():
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+    ''')
+
+    
+    conn.commit()
+    conn.close()
