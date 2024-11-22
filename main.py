5cf10835b50d6769074125496717e8ff609b466e
Veronika <begimotik220@gmail.com>
2024-11-23 02:09:50 +0300

    pages

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from db import create_tables, delete_tables
-from router import router as users_router
-
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import HTMLResponse
@@ -11,15 +8,12 @@ from starlette.requests import Request
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await delete_tables()
     print("База очищена")
-    await create_tables()
     print("База готова к работе")
     yield
     print("Выключение")
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(users_router)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -28,9 +22,6 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("landing.html", {"request": request})
-@app.get("/registration/password", response_class=HTMLResponse)
-async def registration(request: Request):
-    return templates.TemplateResponse("reg_password.html", {"request": request})
 @app.get("/registration", response_class=HTMLResponse)
 async def registration(request: Request):
     return templates.TemplateResponse("reg.html", {"request": request})
