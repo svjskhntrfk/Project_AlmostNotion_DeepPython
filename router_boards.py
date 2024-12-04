64dc62195da9a95dd44c0b0c4c142c68525c45ca
siehfis <mdfilippovich@edu.hse.ru>
2024-12-04 22:19:41 +0300

    govnokod

--- router_boards.py
+++ router_boards.py
@@ -14,16 +14,21 @@ router = APIRouter(
     tags=["Board"]
 )
 @router.post("/main_page/{user_id}/add_board")
-async def create_new_board(user_id: str, title : str) :
+async def create_new_board(user_id: str, title : str = Form()) :
     board_id = await create_board(int(user_id), str(title))
     return RedirectResponse("/board/main_page/" + user_id + '/' + str(board_id) ,
         status_code=status.HTTP_302_FOUND)
 
-@router.get("/main_page/{user_id}/{board_id}")
-async def board_page(user_id: str, board_id: str, request: Request):
-    board = await get_board_by_user_id_and_board_id(int(user_id), int(board_id))
-    print(board)
-    return templates.TemplateResponse("article.html", {"request": request, "user_id": user_id, "board_id":board_id, "texts" : board["texts"]})
+@router.get("/main_page/{user_id}/add_board")
+async def board_title_page(request: Request):
+    return templates.TemplateResponse("article.html", {"request": request})
+
+
+#@router.get("/main_page/{user_id}/{board_id}")
+#async def board_page(user_id: str, board_id: str, request: Request):
+#    board = await get_board_by_user_id_and_board_id(int(user_id), int(board_id))
+#    print(board)
+#    return templates.TemplateResponse("article.html", {"request": request, "user_id": user_id, "board_id":board_id, "texts" : board["texts"]})
 
 
 @router.post("/main_page/{user_id}/{board_id}/add_text")
@@ -32,3 +37,7 @@ async def add_text_on_board(user_id: str, board_id: str, text = Form()) :
     print(text_id)
     return RedirectResponse("/board/main_page/" + user_id + '/' + board_id ,
         status_code=status.HTTP_302_FOUND)
+
+@router.get("/main_page/{user_id}/{add_board/add_text")
+async def board_text_page(request: Request):
+    return templates.TemplateResponse("board_text.html", {"request": request})
