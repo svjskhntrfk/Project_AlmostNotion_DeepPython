298ade1172bb3dc3bff73055ff3adbeaa0cb0f36
siehfis <mdfilippovich@edu.hse.ru>
2024-12-07 15:32:11 +0300

    working_profile

--- router_profile.py
+++ router_profile.py
@@ -14,12 +14,12 @@ router = APIRouter(
     tags=["Profile"]
 )
 
-@router.post("/main_page/profile/{user_id}")
-async def profile_page(user_id : int, first_name = Form(), last_name = Form(), age = Form()) :
-    user_dict = {"user_id" : user_id, "first_name" : first_name, "last_name" : last_name, "age" : age}
-    await add_profile_data(**user_dict)
-    return RedirectResponse("/main_page/profile" + str(user_id),
-                            status_code=status.HTTP_302_FOUND)
+#@router.post("/main_page/profile/{user_id}")
+#async def profile_page(user_id : int, first_name = Form(), last_name = Form(), age = Form()) :
+#    user_dict = {"user_id" : user_id, "first_name" : first_name, "last_name" : last_name, "age" : age}
+#    await add_profile_data(**user_dict)
+#    return RedirectResponse("/main_page/profile" + str(user_id),
+#                            status_code=status.HTTP_302_FOUND)
 
 @router.get("/main_page/profile/{user_id}", response_class=HTMLResponse)
 async def profile_page(user_id: str, request: Request):
