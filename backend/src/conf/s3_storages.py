ddaefcc6e9ee5c47cd849b0900034cb4c6d6027a
Veronika Dmitrenko <begimotik220@gmail.com>
2024-12-19 02:13:00 +0300

    Revert "Add s3 with jwt"

--- backend/src/conf/s3_storages.py
+++
@@ -1,9 +0,0 @@
-from config import settings
-from backend.src.conf.s3_client import S3StorageManager
-
-
-class MediaStorage(S3StorageManager):
-    bucket_name = settings.MINIO_MEDIA_BUCKET
-    default_acl = 'public-read'
-
-media_storage = MediaStorage()
--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 import os
-from enum import Enum
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
-class ModeEnum(str, Enum):
-    development = "development"
-    production = "production"
-    testing = "testing"
 
 class Settings(BaseSettings):
     DB_USER: str
@@ -14,22 +8,6 @@ class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
-
-    #MODE : str
-    MINIO_ROOT_USER : str
-    MINIO_ROOT_PASSWORD : str
-
-    MINIO_ACCESS_KEY: str
-    MINIO_SECRET_KEY: str
-    MINIO_DOMAIN: str
-    MINIO_REGION_NAME: str
-    MINIO_MEDIA_BUCKET: str
-    MINIO_STATIC_BUCKET: str
-    MINIO_DATABASE_BUCKET: str
-    MINIO_USE_SSL: bool
-
-    #SECRET_KEY: str
-    #ENCRYPT_KEY: str
     API_SECRET: str
     HASH_SALT: str
     JWT_SECRET: str 
@@ -44,4 +22,5 @@ class Settings(BaseSettings):
         return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
                 f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
 
+        
 settings = Settings()
